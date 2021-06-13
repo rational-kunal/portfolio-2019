2d1b7e0217c87fe429a0d75c7e1f7427a97a1641
kunal <kunalgk1999@gmail.com>
2021-06-13 10:38:21 +0530

    Updates

---
+++ precache-manifest.02ed380273a77c8bd8540be1685ecec6.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb9df66375713da7dbba39c3cf6fe1c1",
+    "url": "/portfolio-2019/index.html"
+  },
+  {
+    "revision": "50e7341e92a7fa31ce53",
+    "url": "/portfolio-2019/static/css/main.3492a43a.chunk.css"
+  },
+  {
+    "revision": "93f082380dace2eef1be",
+    "url": "/portfolio-2019/static/js/2.bcc79647.chunk.js"
+  },
+  {
+    "revision": "50e7341e92a7fa31ce53",
+    "url": "/portfolio-2019/static/js/main.34cb21bd.chunk.js"
+  },
+  {
+    "revision": "4a4bf3b1d3b213f00b72",
+    "url": "/portfolio-2019/static/js/runtime-main.64908580.js"
+  },
+  {
+    "revision": "e3033ba3e08196f17dcb793c7830a4d5",
+    "url": "/portfolio-2019/static/media/cpp.e3033ba3.svg"
+  },
+  {
+    "revision": "7efd45239dce20d9177f62054176cc6c",
+    "url": "/portfolio-2019/static/media/django.7efd4523.svg"
+  },
+  {
+    "revision": "a9e55355138c253534d5a8a0398a3a3c",
+    "url": "/portfolio-2019/static/media/document_link.a9e55355.svg"
+  },
+  {
+    "revision": "ea7eb4856180f7ac60545977256457ae",
+    "url": "/portfolio-2019/static/media/express.ea7eb485.svg"
+  },
+  {
+    "revision": "3050429b612362a44bb4add417b96047",
+    "url": "/portfolio-2019/static/media/github.3050429b.svg"
+  },
+  {
+    "revision": "e5757c2346e5b30e585414636668654d",
+    "url": "/portfolio-2019/static/media/github_link.e5757c23.svg"
+  },
+  {
+    "revision": "c259f84a063a26f5f595b7346decbc0c",
+    "url": "/portfolio-2019/static/media/html.c259f84a.svg"
+  },
+  {
+    "revision": "5621ff5ded786dbf12085694c1da48a2",
+    "url": "/portfolio-2019/static/media/intagram.5621ff5d.svg"
+  },
+  {
+    "revision": "71a998296493cab2a0d13f699907466d",
+    "url": "/portfolio-2019/static/media/js.71a99829.svg"
+  },
+  {
+    "revision": "d468637c545b32814ff918fd8983b82f",
+    "url": "/portfolio-2019/static/media/link_link.d468637c.svg"
+  },
+  {
+    "revision": "008d93822337b38f1e1c9b8df114a016",
+    "url": "/portfolio-2019/static/media/linkedin.008d9382.svg"
+  },
+  {
+    "revision": "7eb92d8ebae939818264b736da6408d5",
+    "url": "/portfolio-2019/static/media/python.7eb92d8e.svg"
+  },
+  {
+    "revision": "969e6dcd301783ae33d72d35515881de",
+    "url": "/portfolio-2019/static/media/react.969e6dcd.svg"
+  }
+]);
